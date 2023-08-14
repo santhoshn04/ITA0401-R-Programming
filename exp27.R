19c7bf0ffaadc5f278be394635f6cd685d9b43a4
Santhosh <114389810+santhoshn04@users.noreply.github.com>
2023-08-14 10:44:28 +0530

    Add files via upload

---
+++ exp27.R
@@ -0,0 +1,5 @@
+data("women")
+
+height_factor <- cut(women$height, breaks = c(50, 60, 70, 80), labels = c("Short", "Medium", "Tall"))
+
+head(height_factor)
---
+++ exp28.R
@@ -0,0 +1,7 @@
+set.seed(123)  # For reproducibility
+sampled_letters <- sample(LETTERS, size = 100, replace = TRUE)
+letter_factor <- factor(sampled_letters)
+
+five_levels <- unique(letter_factor)[1:5]
+
+print(five_levels)
